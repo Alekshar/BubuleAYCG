6ef235f019f60fde72098dbf68cccdea88000e79
Nolyurn <Nolyurn@192.168.1.47>
2016-09-27 15:21:46 +0200

    Suppression CoordonneesXYZ Ajout de Point3D

--- src/perso/gatien/Point3D.java
+++ src/perso/gatien/Point3D.java
@@ -1,11 +1,11 @@
 package perso.gatien;
 
-public class CoordonneesXYZ {
+public class Point3D {
 	private double x;
 	private double y;
 	private double z;
 	
-	public CoordonneesXYZ(double x, double y, double z){
+	public Point3D(double x, double y, double z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
@@ -21,5 +21,9 @@ public class CoordonneesXYZ {
 	
 	public double getZ(){
 		return z;
+	}
+	
+	public double distance(Point3D c1){
+		return Math.abs(Math.sqrt( Math.pow(c1.getX()-this.getX(),2)+Math.pow(c1.getY()-this.getY(),2)+Math.pow(c1.getZ()-this.getZ(),2)));
 	}
 }
